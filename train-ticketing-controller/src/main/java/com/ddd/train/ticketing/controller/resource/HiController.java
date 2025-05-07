63d7f8d2b88a4e365b2dcda11148679946023c4c
USER\Anh.KimNgoc <anh.kimngoc@mbs.com.vn>
2025-05-07 17:35:29 +0700

    [controller] code update

--- train-ticketing-controller/src/main/java/com/ddd/train/ticketing/controller/resource/HiController.java
+++ train-ticketing-controller/src/main/java/com/ddd/train/ticketing/controller/resource/HiController.java
@@ -1,6 +1,7 @@
 package com.ddd.train.ticketing.controller.resource;
 
 import com.ddd.train.ticketing.application.service.event.EventAppService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,17 @@ public class HiController {
     private final EventAppService eventAppService;
 
     @GetMapping("/ping")
+    @RateLimiter(name = "backendA", fallbackMethod = "fallbackPing")
     public ResponseEntity<String> ping() {
-
         return ResponseEntity.ok(eventAppService.sayHi("KIM"));
+    }
+
+    public String fallbackPing(String message) {
+        return "TOOOOO";
+    }
+    @GetMapping("/hi")
+    @RateLimiter(name = "backendB", fallbackMethod = "fallbackPing")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok(eventAppService.sayHi("Hi"));
     }
 }
